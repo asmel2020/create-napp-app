32e3f8e1e5e51a47d60e496d42760057b1ce0a0e
Danny Gonzalez <danny.galicia@rhizom.me>
2023-07-06 15:29:29 -0300

    update

--- src/utils/gitClone.ts
+++ src/utils/gitClone.ts
@@ -1,12 +1,8 @@
 import execa from 'execa';
-
+import ora from 'ora';
 export const gitClone = async (nameProject:string) => {
-
-
+	const spinner = ora('Clone and Install').start();
 	await execa('git clone', ['https://github.com/asmel2020/template-nest.git',nameProject]);
-	await execa('cd', [`${nameProject}`]);
-	await execa('npm install');
-	await execa('git remote rm origin');
-	//await execa("git", ["https://github.com/asmel2020/template-nest.git"]);
+	spinner.succeed('Clone and Install');
 }
 
