253bdef2d477cc67bcc5defc3e58368c98a0d28a
Danny Gonzalez <danny.galicia@rhizom.me>
2023-07-06 16:42:55 -0300

    bug: fix duplicado

--- src/utils/gitClone.ts
+++ src/utils/gitClone.ts
@@ -1,7 +1,18 @@
 import execa from 'execa';
+import path from 'path';
 import ora from 'ora';
+import * as fs from 'fs';
 export const gitClone = async (nameProject:string) => {
 	const spinner = ora('Clone and Install').start();
+
+
+	const isExistsProjects=fs.existsSync(path.resolve(nameProject));
+	if(isExistsProjects){
+		spinner.fail('The project name already exists');
+		return
+	}
+	console.log('nameProject',nameProject);
+
 	await execa('git clone', ['https://github.com/asmel2020/template-nest.git',nameProject]);
 	spinner.succeed('Clone and Install');
 }
--- src/utils/nameProjectInput.ts
+++ src/utils/nameProjectInput.ts
@@ -1,24 +1,31 @@
-import inquirer from 'inquirer';
-
+import inquirer from "inquirer";
+import path from "path";
+import * as fs from "fs";
 const leer = [
-	{
-		type: 'input',
-		message: 'Enter project name',
-		name: 'nameProject',
-		default: 'back-end',
-		validate(value:any) {
-			console.log(value);
-			if (value.length===0) {
-				return 'Please enter your name';
-			} 
-			return true;
-		}
-	}
-]
+  {
+    type: "input",
+    message: "Enter project name",
+    name: "nameProject",
+    default: "back-end",
+    placeholder: "Enter project name",
+    validate(value: any) {
 
+      if (value.length === 0) {
+        return "Please enter your name";
+      }
 
+      const isExistsProjects = fs.existsSync(path.resolve(value));
 
-export const nameProjectInput = async ():Promise<string> => {
-	const { nameProject }:{nameProject:string} = await inquirer.prompt(leer);
-	return nameProject;
-}
+      if (isExistsProjects) {
+        return "The project name already exists";
+      }
+
+      return true;
+    },
+  },
+];
+
+export const nameProjectInput = async (): Promise<string> => {
+  const { nameProject }: { nameProject: string } = await inquirer.prompt(leer);
+  return nameProject;
+};
