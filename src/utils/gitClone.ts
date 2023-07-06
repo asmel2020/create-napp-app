53bad64fd4238f1c321bff51f6f830b8800eb6cc
Danny Gonzalez <danny.galicia@rhizom.me>
2023-07-06 17:16:19 -0300

    feat: add new description

--- src/utils/gitClone.ts
+++ src/utils/gitClone.ts
@@ -3,7 +3,8 @@ import path from 'path';
 import ora from 'ora';
 import * as fs from 'fs';
 export const gitClone = async (nameProject:string) => {
-	const spinner = ora('Clone and Install').start();
+	console.log();
+	const spinner = ora('Installation in progress... ☕').start();
 
 
 	const isExistsProjects=fs.existsSync(path.resolve(nameProject));
@@ -12,8 +13,17 @@ export const gitClone = async (nameProject:string) => {
 		spinner.fail('The project name already exists');
 		return
 	}
-	
+
 	await execa('git clone', ['https://github.com/asmel2020/template-nest.git',nameProject]);
-	spinner.succeed('Clone and Install');
+
+	spinner.succeed();
+	console.log();
+	console.log(`🚀  Successfully created project ${nameProject}`);
+	console.log(`👉  Get started with the following commands:`);
+	console.log();
+	console.log(`$ cd ${nameProject}`);
+	console.log('$ npm install or yarn');
+	console.log('$ yarn run start');
+	console.log();
 }
 
