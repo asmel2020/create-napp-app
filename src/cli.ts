b5acd5b35e584d5b5e6a612030702bd920678cd7
Danny Gonzalez <danny.galicia@rhizom.me>
2023-07-06 21:25:28 -0300

    feat: new version

--- src/cli.ts
+++ src/cli.ts
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
-import inquirer from "inquirer";
-import { gitClone, nameProjectInput, questions } from "./utils";
+import inquirer from 'inquirer';
+import { gitClone, nameProjectInput, questions } from './utils';
 
 const startProcess = async () => {
-  const { templateRepository } = await inquirer.prompt(questions);
-  const nameProject = await nameProjectInput();
-  gitClone(templateRepository, nameProject);
+	const { templateRepository } = await inquirer.prompt(questions);
+	const nameProject = await nameProjectInput();
+	gitClone(templateRepository, nameProject);
 };
 
 startProcess();
--- src/utils/listRepository.ts
+++ src/utils/listRepository.ts
@@ -1,4 +1,4 @@
 export const listRepository:{[key:string]:string} =  {
-    '1':'https://github.com/nestjs/typescript-starter.git',
-    '2':'https://github.com/asmel2020/NAPP-REST.git',
+	'1':'https://github.com/nestjs/typescript-starter.git',
+	'2':'https://github.com/asmel2020/NAPP-REST.git',
 }
